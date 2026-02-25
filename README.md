d7ac6c4076372d89cb2f90c3fa915a3aa2131481
sarfarazstark <sarfarazstark@gmail.com>
2026-02-26 03:28:29 +0530

    feat: update preview image path to public assets and reduce transition duration

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 </p>
 
 <p align="center">
-  <img src="./preview.jpg" alt="Finco Preview" width="720" />
+  <img src="./public/assets/images/preview.png" alt="Finco Preview" width="720" />
 </p>
 
 ---
--- components/layout/TransitionLayout.tsx
+++ components/layout/TransitionLayout.tsx
@@ -45,7 +45,7 @@ export default function TransitionLayout({
 				animate="animate"
 				exit="exit"
 				variants={variants}
-				transition={{ duration: 0.35, ease: 'easeInOut' }}
+				transition={{ duration: 0.1, ease: 'easeInOut' }}
 				className="h-full min-h-0 min-w-0 overflow-y-auto overflow-x-hidden"
 			>
 				<FrozenRouter>{children}</FrozenRouter>
